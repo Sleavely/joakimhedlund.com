70e22186dc3e3f0b7277a05e38dfd61cecd911eb
Joakim Hedlund <contact@joakimhedlund.com>
2015-03-17 12:53:32 +0100

    Use Drunken Parrot theme in backend

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
 
-	<link href="/css/app.css" rel="stylesheet">
-
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+  <link href="{{{ asset('css/bootstrap.css') }}}" rel="stylesheet" type="text/css" />
+  <link href="{{{ asset('css/font-awesome.min.css') }}}" rel="stylesheet" type="text/css" />
+  <link href="{{{ asset('css/drunken-parrot.css') }}}" rel="stylesheet" type="text/css" />
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
