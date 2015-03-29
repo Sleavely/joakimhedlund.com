dc3e315564bc092cf30d429b371f228946db8a03
Joakim Hedlund <contact@joakimhedlund.com>
2015-03-29 09:18:36 +0200

    Add published_on column

---
+++ database/migrations/2015_03_29_071614_blogpost_published_on.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BlogpostPublishedOn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('blog_posts', function($table)
+		{
+	    $table->timestamp('published_on');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('blog_posts', function($table)
+		{
+		  $table->dropColumn('published_on');
+		});
+	}
+
+}
