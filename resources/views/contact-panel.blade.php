3a844d6c62f31187c79cf6f55c4cda566ae79a68
Joakim Hedlund <contact@joakimhedlund.com>
2016-01-06 13:55:56 +0100

    Common template for contact-buttons

---
+++ resources/views/contact-panel.blade.php
@@ -0,0 +1,8 @@
+<div id="contact" class="panel panel-default">
+  <div class="panel-body" style="text-align: center; padding-bottom: 3em;">
+    <h2>Contact</h2>
+    <a href="https://twitter.com/Sleavely" class="btn btn-embossed btn-primary" style="width: 100px;">Twitter</a>
+    <a href="https://www.linkedin.com/in/joakimhedlund" class="btn btn-embossed btn-primary" style="width: 100px;">LinkedIn</a>
+    <a href="mailto:contact@joakimhedlund.com" class="btn btn-embossed btn-primary" style="width: 100px;">E-mail</a>
+  </div>
+</div>
--- resources/views/portfolio/archive.blade.php
+++ resources/views/portfolio/archive.blade.php
@@ -101,16 +101,9 @@
       </div>
     </div>
 
-    <div class="row" id="contact">
+    <div class="row">
       <div class="col-sm-6 col-sm-offset-3">
-        <div class="panel panel-default">
-          <div class="panel-body" style="text-align: center; padding-bottom: 3em;">
-            <h2>Contact</h2>
-            <a href="https://twitter.com/Sleavely" class="btn btn-embossed btn-primary" style="width: 100px;">Twitter</a>
-            <a href="https://www.linkedin.com/in/joakimhedlund" class="btn btn-embossed btn-primary" style="width: 100px;">LinkedIn</a>
-            <a href="mailto:contact@joakimhedlund.com" class="btn btn-embossed btn-primary" style="width: 100px;">E-mail</a>
-          </div>
-        </div>
+        @include('contact-panel')
       </div>
     </div>
 
