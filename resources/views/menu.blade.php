e03627961ed2b20afeb40e4904d48a6a45592c62
Joakim Hedlund <contact@joakimhedlund.com>
2016-01-06 15:14:04 +0100

    Only show blog to logged-in Jocke

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,7 +5,9 @@
       <ul class="nav navbar-nav">
         <li class="{{ (Request::path() == '/' ? ' active' : '') }}"><a href="/">Home</a></li>
         <li class="{{ (starts_with(Request::path(), 'portfolio') ? ' active' : '') }}"><a href="/portfolio">Portfolio</a></li>
+        @if(Auth::check())
         <li class="{{ (starts_with(Request::path(), 'blog') ? ' active' : '') }}"><a href="/blog">Blog</a></li>
+        @endif
       </ul>
     </div>
   </div>
