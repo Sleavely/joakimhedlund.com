a59d52c9c0dc3877208c0d34cc9f600cd3003956
Joakim Hedlund <contact@joakimhedlund.com>
2016-06-05 11:28:29 +0200

    Make the blog public

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,9 +5,7 @@
       <ul class="nav navbar-nav">
         <li class="{{ (Request::path() == '/' ? ' active' : '') }}"><a href="/">Home</a></li>
         <li class="{{ (starts_with(Request::path(), 'portfolio') ? ' active' : '') }}"><a href="/portfolio">Portfolio</a></li>
-        @if(Auth::check())
         <li class="{{ (starts_with(Request::path(), 'blog') ? ' active' : '') }}"><a href="/blog">Blog</a></li>
-        @endif
       </ul>
     </div>
   </div>
