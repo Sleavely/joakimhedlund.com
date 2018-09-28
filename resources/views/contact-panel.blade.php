fe6931f5cf52c901f0e89b387bc98dc28aad2447
Joakim Hedlund <contact@joakimhedlund.com>
2018-09-28 12:40:01 +0000

    RIP Twitter. Long live Github.

--- resources/views/contact-panel.blade.php
+++ resources/views/contact-panel.blade.php
@@ -1,7 +1,7 @@
 <div id="contact" class="panel panel-default">
   <div class="panel-body" style="text-align: center; padding-bottom: 3em;">
     <h2>Contact</h2>
-    <a href="https://twitter.com/Sleavely" class="btn btn-embossed btn-primary" style="width: 100px;">Twitter</a>
+    <a href="https://github.com/Sleavely" class="btn btn-embossed btn-primary" style="width: 100px;">Github</a>
     <a href="https://www.linkedin.com/in/joakimhedlund" class="btn btn-embossed btn-primary" style="width: 100px;">LinkedIn</a>
     <a href="mailto:contact@joakimhedlund.com" class="btn btn-embossed btn-primary" style="width: 100px;">E-mail</a>
   </div>
